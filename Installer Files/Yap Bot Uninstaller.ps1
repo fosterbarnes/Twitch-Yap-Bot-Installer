865c3c32a1fd4bd1ab94a03d47611d1e9a580f68
fosterbarnes <152516585+fosterbarnes@users.noreply.github.com>
2024-08-23 16:40:02 -0400

    Add files via upload

---
+++ Installer Files/Yap Bot Uninstaller.ps1
@@ -0,0 +1,3 @@
+﻿Remove-Item -Path "$env:USERPROFILE\Documents\Applications\Yap Bot" -Recurse -Force -Confirm:$false
+Remove-Item -Path "$env:USERPROFILE\Desktop\Twitch Yap Bot.lnk" -Recurse -Force -Confirm:$false
+Remove-Item -Path "$env:USERPROFILE\Documents\Pinned Folders\Start Menu Programs\Twitch Yap Bot.lnk" -Recurse -Force -Confirm:$false
